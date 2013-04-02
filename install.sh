8234ff0983f6b18dae40543e03cd16f7a76f5285
nixargh <nixargh@gmail.com>
2013-04-03 01:22:20 +0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#### INFO ######################################################################
+# (*w) 
+VERSION=2
+#### SETTINGS ##################################################################
+BIN='conserve'
+CONFDIR='/etc/conserve'
+BINDIR='/usr/sbin'
+INFORM='inform.conf'
+CRED='cred'
+LOGROTATE='conserve.lr'
+USER=`/usr/bin/env |grep -c USER=root`
+#### PROGRAM ###################################################################
+function copyconf {
+	CONFIG=$1
+	if test -e ./$CONFIG; then
+		if test -e $CONFDIR/$CONFIG; then
+			mv $CONFDIR/$CONFIG $CONFDIR/$CONFIG.bak
+			echo "	$CONFDIR/$CONFIG moved to $CONFDIR/$CONFIG.bak"
+		fi
+		cp ./$CONFIG $CONFDIR
+		echo "	./$CONFIG copied to $CONFDIR/"
+		chmod 660 $CONFDIR/$CONFIG
+		chown root:root $CONFDIR/$CONFIG
+	fi
+}
+
+function install {
+	echo -e "\033[1m Installing... \033[0m"
+	if test ! -d $CONFDIR; then
+		mkdir $CONFDIR
+	fi
+	chmod 770 $CONFDIR
+	cp -f ./$BIN $BINDIR/conserve
+	echo "	./$BIN copied to $BINDIR/conserve"
+	chmod 755 $BINDIR/conserve
+	chown root:root $BINDIR/conserve
+	copyconf $INFORM
+	copyconf $CRED
+	CONFDIR='/etc/logrotate.d'
+	copyconf $LOGROTATE
+	echo -e "\033[1m OK. \033[0m"
+}
+
+if [ "$USER" != "0" ]; then
+	if test -e $BINDIR/conserve; then
+		echo -ne "\033[1m $BINDIR/conserve exist, replace it? [y|n]:  \033[0m"
+		read -a ANSWER
+		if [ $ANSWER == 'y' ];then
+			install
+		elif [ $ANSWER == 'n' ];then
+			echo "Exiting."
+			exit 0;
+		else
+			echo "Bad answer \"$ANSWER\". Exiting."
+			exit 1;
+		fi
+	else
+		install
+		exit 0;
+	fi
+else
+  echo -e "  \033[1m login as root first! \033[0m"
+  exit 1;
+fi
