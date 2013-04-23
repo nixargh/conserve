5d8686595a3a07b621a675cd3d40a43f59d69aa7
nixargh <nixargh@gmail.com>
2013-04-23 13:14:28 +0400

    Collect#get_md_info compleated

--- lib/Collector.rb
+++ lib/Collector.rb
@@ -13,17 +13,18 @@ class Collector
 		lvm['info_file'] = "#{@info_dir}/lvm"
 		mount['info_file'] = "#{@info_dir}/mount"
 
+		# Hash of creatures information hashes
 		@creatures = { 'hdd' => hdd, 'md' => md, 'partition' => partition, 'lvm' => lvm, 'mount' => mount }
 	end
 
-	def collect
+	def collect # collect iformation about creatures
 		list_disks!
 		@creatures.each_key{|creature|
 			@creatures[creature]['info'] = eval("get_#{creature}_info")
 		}
 	end
 
-	def save
+	def save # save information about creatures
 		@creatures.each{|key, value|
 			puts "#{key} = #{value}"
 		}
@@ -31,45 +32,68 @@ class Collector
 
 	private
 
-	def get_hdd_info
+	def get_hdd_info # collect information about phisical disk devices
 		hdd_list = Array.new
 		@disk_list.each{|disk, size|
-			if disk =~ /\A\/dev\/(s|h)d[a-z]\z/
+			if disk =~ /\A\/dev\/[sh]d[a-z]\z/
 				hdd = Hash.new
 				hdd['name'] = disk
-				disk['size'] = size
+				hdd['size'] = size 
+				hdd['blocksize'] = get_device_blocksize(disk)
 				hdd_list.push(hdd)
 			end
 		}
 		hdd_list
 	end
 
-	def get_md_info
-		'md'
+	def get_md_info # collect information about software RAID devices
+		md_list = Hash.new
+		@disk_list.each{|disk, size|
+			if disk =~ /\A\/dev\/md\d{1,3}\z/
+				md_list['name'] = disk
+				`cat /proc/mdstat`.each_line{|line|
+					if line.index(File.basename(disk))
+						line.scan(/raid\d{1,2}/){|raid_lvl| 
+							md_list['raid_lvl'] = raid_lvl.match(/\d{1,2}/)[0].to_i
+						}
+						md_list['members'] = Array.new
+						line.scan(/[hs]d[a-z]{1,2}\d/){|disk|
+							md_list['members'].push("/dev/#{disk}")
+						}
+					end
+				}
+			end	
+		}
+		md_list
 	end
 
-	def get_partition_info
+	def get_partition_info # collect information about partition tables
 	end
 
-	def get_lvm_info
+	def get_lvm_info # collect information about LVM
 	end
 
-	def get_mount_info
+	def get_mount_info # collect information about how to mount partitions
 	end
 
-	def list_disks!
+	def list_disks! # create list of disk devices on current machine
 		@disk_list = Hash.new
 		`fdisk -l 2>/dev/null`.each_line{|line|
 			if line.index('Disk /')
 				dev, size = line.split(':')
-				dev = dev.split(' ')[1]
-				size = size.split(' ')[-2]
-				@disk_list[dev] = size
+				dev = dev.split(' ')[1]			# device name
+				size = size.split(' ')[-2] 		# size in bytes
+				@disk_list[dev] = size.to_i
 			end
 		}
 	end
+
+	def get_device_blocksize(dev) # get blocksize of device in bytes
+		`blockdev --getbsz #{dev}`.chomp.to_i
+	end
 end
 
+# Test section
 collector = Collector.new
 collector.collect
 collector.save
